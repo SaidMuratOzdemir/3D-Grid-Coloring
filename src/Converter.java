f78d48f545b160f0cb7ddb1f045dc5dc97f3bfcb
SaidMuratOzdemir <114745121+SaidMuratOzdemir@users.noreply.github.com>
2024-05-10 13:05:22 +0300

    ref: deleted convert function from 2d to graph

--- src/Converter.java
+++ src/Converter.java
@@ -2,38 +2,6 @@ import java.util.ArrayList;
 
 public class Converter {
 
-
-    public static Graph convert(char[][] array) {
-        Graph graph = new Graph();
-        for (char[] chars : array) {
-            for (char aChar : chars) {
-                ArrayList<Node> neighbours = new ArrayList<>();
-                Node node = new Node(aChar, neighbours);
-                graph.addNode(node);
-            }
-        }
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                Node node = graph.getNodes().get(i * array[i].length + j);
-                if (i - 1 >= 0) {
-                    node.getNeighbors().add(graph.getNodes().get((i - 1) * array[i].length + j));
-                }
-                if (j - 1 >= 0) {
-                    node.getNeighbors().add(graph.getNodes().get(i * array[i].length + j - 1));
-                }
-                if (j + 1 < array[i].length) {
-                    node.getNeighbors().add(graph.getNodes().get(i * array[i].length + j + 1));
-                }
-                if (i + 1 < array.length) {
-                    node.getNeighbors().add(graph.getNodes().get((i + 1) * array[i].length + j));
-                }
-
-            }
-        }
-        return graph;
-    }
-
     public static Graph convert(char[][][] array) {
         Graph graph = new Graph();
         int depth = array.length;
@@ -44,7 +12,7 @@ public class Converter {
             for (int j = 0; j < height; j++) {
                 for (int k = 0; k < width; k++) {
                     ArrayList<Node> neighbours = new ArrayList<>();
-                    Node node = new Node(array[i][j][k], neighbours);
+                    Node node = new Node(i, j, k, array[i][j][k], neighbours);
                     graph.addNode(node);
                 }
             }
