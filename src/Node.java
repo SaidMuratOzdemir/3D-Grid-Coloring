e704960c3326f1ca21b8195376c2cc2037920ea3
SaidMuratOzdemir <114745121+SaidMuratOzdemir@users.noreply.github.com>
2024-05-10 13:20:51 +0300

    imp: added depth, height and width properties

--- src/Node.java
+++ src/Node.java
@@ -2,14 +2,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
+    private int depth;
+    private int height;
+    private int width;
+    private double color;
     private final char value;
     private final List<Node> neighbors;
-    private int color;
 
-    public Node(char value, ArrayList<Node> neighbors) {
+    public Node(int depth, int height, int width, char value, ArrayList<Node> neighbors) {
+        this.depth = depth;
+        this.height = height;
+        this.width = width;
         this.neighbors = neighbors;
         this.value = value;
         this.color = 0;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public double getColor() {
+        return color;
+    }
+
+    public void setColor(double color) {
+        this.color = color;
     }
 
     public char getValue() {
@@ -20,19 +58,14 @@ public class Node {
         return neighbors;
     }
 
-    public int getColor() {
-        return color;
-    }
-
-    public void setColor(int color) {
-        this.color = color;
-    }
-
     @Override
     public String toString() {
-        return "{" +
-                "value=" + getValue() +
-                ", color=" + getColor() +
+        return "Node{" +
+                "depth=" + depth +
+                ", height=" + height +
+                ", width=" + width +
+                ", color=" + color +
+                ", value=" + value +
                 '}';
     }
 }
